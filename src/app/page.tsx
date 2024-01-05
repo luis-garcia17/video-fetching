eade5ec235808adbbb80aea1163d1a4c6324b77a
Luis Garcia <luis.lag17@gmail.com>
2024-01-05 02:39:24 -0600

    style: added design for tablet and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 "use client";
 import { useState, useEffect } from "react";
-import Image from "next/image";
-import { FaVideo, FaRegSquareCaretRight, FaClapperboard } from "react-icons/fa6";
+import { FaVideo, FaClapperboard } from "react-icons/fa6";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  /*
-  - Fetch a list of all the videos
-
-  */
-
   interface Video {
     id: string;
     title: string;
@@ -20,21 +15,20 @@ export default function Home() {
     updated_at: string;
   }
 
-  interface AppError {
-    name: string;
-    message: string;
-    stack?: string;
-  }
 
   const [videos, setVideos] = useState<Video[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
+  const router = useRouter();
+  
   useEffect(() => {
     (async () => {
       try {
         setLoading(true);
-        const response = await fetch("/api/videos");
+        const response = await fetch("/api/videos")
 
         if (!response.ok) {
           throw new Error("Failed to fetch data");
@@ -56,8 +50,15 @@ export default function Home() {
 
   return (
     <main className="bg-stone-50">
-      
-      <header className="h-12 bg-red-600 flex justify-left items-center text-white">
+      <header 
+        className="
+          h-12 
+          bg-red-600 
+          flex 
+          justify-left 
+          items-center 
+          text-white"
+      >
         <FaVideo className="ml-5 text-3xl" />
         <h1 className="ml-3 text-left text-lg">MyTube</h1>
       </header>
@@ -65,27 +66,39 @@ export default function Home() {
       {error && <p>Error: {error}</p>}
 
       {videos.length > 0 && (
-        <ul>
+        <ul className="lg:flex lg:flex-wrap lg:justify-around lg:gap-x-4">
           {videos.map(video => (
-            <li 
+            <li
               key={video.id}
-              className="w-4/5 mx-auto my-6 rounded-md shadow-md overflow-hidden flex flex-col text-gray-600"
+              className="
+                w-4/5 
+                md:w-3/4
+                lg:w-1/6
+                mx-auto 
+                my-6 
+                rounded-md 
+                shadow-md 
+                overflow-hidden 
+                flex 
+                flex-col 
+                md:flex-row
+                lg:flex-col
+                text-gray-600
+              "
             >
               <iframe
                 title={video.title}
-                // width="560"
-                // height="315"
                 src={video.url}
-                frameBorder="0"
                 allow="accelerometer autoplay clipboard-write encrypted-media gyroscope picture-in-picture"
                 allowFullScreen
               ></iframe>
-              <div className="flex justify-left items-center gap-x-2 ml-2 mt-2">
-                <FaClapperboard className="text-2xl" />
-                <strong>{video.title}</strong> 
+              <div className="flex flex-col justify-center md:items-center md:w-full lg:items-start gap-y-2">
+                <div className="flex justify-left items-center gap-x-2 ml-2 mt-2">
+                  <FaClapperboard className="text-2xl" />
+                  <strong>{video.title}</strong>
+                </div>
+                <p className="text-sm ml-2 mb-2">{video.author}</p>
               </div>
-              <p className="text-sm ml-2 mb-2">{video.author}</p>
-              {/* <small>{video.release_date}</small> */}
             </li>
           ))}
         </ul>
